453f48c4ff0771fbe43fac056315e205b2078f48
duongdat98 <duongdat@coders.com>
2020-09-19 19:41:14 +0700

    hj

--- client/src/App.js
+++ client/src/App.js
@@ -6,37 +6,50 @@ import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
-import { loadUser } from './actions/auth'
+import { loadUser } from './actions/auth';
 import store from './store';
 import { Provider } from 'react-redux';
 import setAuthToken from './utils/setAuthToken';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 
-if(localStorage.token){
-  setAuthToken(localStorage.token)
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
 
 const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
-  }, [])
+  }, []);
 
   return (
     <Provider store={store}>
       <Router>
         <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
           <Alert />
           <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute
+              exact
+              path='/create-profile'
+              component={CreateProfile}
+            />
+            <PrivateRoute
+              exact
+              path='/edit-profile'
+              component={EditProfile}
+            />
           </Switch>
         </section>
       </Router>
     </Provider>
-  )
-}
+  );
+};
 
-
-
-export default App
+export default App;
